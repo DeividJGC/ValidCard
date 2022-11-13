b7ef5e6d9e20fd580adf8baf5eb6ca3519ff5b5e
DeividJGC <116898295+DeividJGC@users.noreply.github.com>
2022-11-14 00:12:11 +0100

    Add files via upload

--- Validcard.py
+++ Validcard.py
@@ -1,12 +1,16 @@
-print("  _____           _           _       _                 _    _____    _____ 	 ")
-print(" |  __ \         (_)         (_)     | |               | |  / ____|  / ____|  ")
-print(" | |  | |   ___   _  __   __  _    __| |               | | | |  __  | |       ")
-print(" | |  | |  / _ \ | | \ \ / / | |  / _` |           _   | | | | |_ | | |       ")
-print(" | |__| | |  __/ | |  \ V /  | | | (_| |          | |__| | | |__| | | |____   ")
-print(" |_____/   \___| |_|   \_/   |_|  \__,_|           \____/   \_____|  \_____|  ")
-print("                                        ______                                ")
-print("                                       |______|                               ")
-print("Creado por Deivid_JGC :)")
+#Importar
+import random
+#Créditos
+print("\033[1;31m " + "  _____           _           _       _                 _    _____    _____ 	 ")
+print("\033[1;31m " + " |  __ \         (_)         (_)     | |               | |  / ____|  / ____|  ")
+print("\033[1;33m " + " | |  | |   ___   _  __   __  _    __| |               | | | |  __  | |       ")
+print("\033[1;33m " + " | |  | |  / _ \ | | \ \ / / | |  / _` |           _   | | | | |_ | | |       ")
+print("\033[1;31m " + " | |__| | |  __/ | |  \ V /  | | | (_| |          | |__| | | |__| | | |____   ")
+print("\033[1;31m " + " |_____/   \___| |_|   \_/   |_|  \__,_|           \____/   \_____|  \_____|  ")
+print("\033[1;31m " + "                                        ______                                ")
+print("\033[1;31m " + "                                       |______|                               ")
+print("\033[1;32m " + "Creado por Deivid_JGC :)")
+#Código de Validación de Números de tarjeta
 def validcard(N):
     T=""; par=0;impar=0;X=""
     if not N.isdigit():return 1,""
@@ -21,7 +25,7 @@ def validcard(N):
         impar+=int(N[c])
 
     if (par+impar)%10!=0:return 3,""
-
+#Código de Validación de Tipo de Tarjeta
     if int(N[0:2])>49 and int(N[0:2])<56:T="**Mastercard"
     if N[0:2]=="34" or N[0:2]=="37":T="**America Express"
     if N[0]=="4":T="**VISA"
@@ -30,12 +34,14 @@ def validcard(N):
     return 4,T
 
 msg=(0,"")
-
+#Mensajes de la Terminal
 while msg[0]!=4:
-    msg=validcard(input("Enter Credit card number 13-19:"))
-    if msg[0]==1:print("must be only numbers 0 - 9")
-    if msg[0]==2:print("must be 13 or more(less or equal 19)")
-    if msg[0]==3:print("Número de tarjeta INVALIDO!!")
+    msg=validcard(input("\033[33m " + "Inserta un número de tarjeta de mínimo 13 y máximo o exáctamente 19 dígitos: "))
+    if msg[0]==1:print("\033[1;31m " + "Solo puede usar números del 0 al 9 ")
+    if msg[0]==2:print("\033[1;31m " + "Solo mínimo 13 y máximo o exáctamente 19 dígitos")
+    if msg[0]==3:print("\033[1;31m " + "Número de tarjeta INVALIDO!!")
 
-print("Tarjeta VALIDA!!")
+print("\033[1;32m " + "Tarjeta VALIDA!!")
 print("Tipo:"+msg[1])
+
+#Muchas Gracias por usar mi script. <3
---
+++ readme.txt
@@ -0,0 +1,28 @@
+ _____           _           _       _                 _    _____    _____ 	 
+|  __ \         (_)         (_)     | |               | |  / ____|  / ____|  
+| |  | |   ___   _  __   __  _    __| |               | | | |  __  | |       
+| |  | |  / _ \ | | \ \ / / | |  / _` |           _   | | | | |_ | | |       
+| |__| | |  __/ | |  \ V /  | | | (_| |          | |__| | | |__| | | |____   
+|_____/   \___| |_|   \_/   |_|  \__,_|           \____/   \_____|  \_____|  
+                                         ______                                
+                                        |______|                               
+Creado por Deivid_JGC
+Marketing IG: dave.code1
+Personal IG: Deivid_JGC
+Github: Deivid_JGC
+
+( Tiempo estimado en crear el script: 2 días)
+
+Esto es un VALIDADOR de tarjetas de crédito, NO es un creador de tarjetas. 
+Es decir...
+Si se introducen mínimo 13 y máximo 19 dígitos el script podrá adivinar depende del BIN, que tipo de tarjeta es, y si es Válida.
+
+BIN: Son los seis primeros dígitos de una tarjeta bancaria.
+
+Muchas Gracias por usar mi script.  <3
+
+
+
+
+Deivid_JGC
+dave.code1
